c25d12a691f6aa9fe066f059ad6eed01351d5ee6
matthiaspaul1234 <mmpaulmkp@gmail.com>
2019-06-19 09:55:37 +0200

    hallo

--- HOOFDCODE_AGV/HOOFDCODE_AGV.ino
+++ HOOFDCODE_AGV/HOOFDCODE_AGV.ino
@@ -30,24 +30,30 @@
 #define tofrechtsboven 16
 #define toflinksboven 17
 #define toflinksonder 18
-
+#define bocht 105
 #define motorrechtsdirection 6
 #define motorlinksdirection 4
-#define motorrechtsstep 3
-#define motorlinksstep 5
+#define motorrechtsstep 5
+#define motorlinksstep 3
+#define buzzer 99
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
                          OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 long durationleft;
 long durationfront;
 long durationright;
 long durationback;
+unsigned long timing;
+unsigned long current;
 int distanceleft;
 int distancefront;
 int distanceright;
 int distanceback;
 int i = 0;
+void noodstopmode();
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+
   pinMode(trigPinleft, OUTPUT);
   pinMode(echoPinleft, INPUT);
   pinMode(trigPinfront, OUTPUT);
@@ -72,14 +78,16 @@ void setup() {
   pinMode(motorrechtsstep, OUTPUT);
   pinMode(motorlinksdirection, OUTPUT);
   pinMode(motorlinksstep, OUTPUT);
-  display.begin(SSD1306_SWITCHCAPVCC);
-  display.clearDisplay();
-  display.display();
-  display.setTextSize(3);  //Select the size of the text
-  display.setTextColor(WHITE);  //for monochrome display only whit is possible
-  display.setCursor(64, 30); //0,0 is the top left corner of the OLED screen
-  display.print(10); //Write the variable to be displayed
-  display.display();
+  pinMode(buzzer, OUTPUT);
+  /* display.begin(SSD1306_SWITCHCAPVCC);
+    display.clearDisplay();
+    display.display();
+    display.setTextSize(3);  //Select the size of the text
+    display.setTextColor(WHITE);  //for monochrome display only whit is possible
+    display.setCursor(64, 30); //0,0 is the top left corner of the OLED screen
+    display.print(10); //Write the variable to be displayed
+    display.display();
+  */
   attachInterrupt(noodstop, noodstopmode, LOW);
 }
 
@@ -90,27 +98,7 @@ void loop() {
     case 0: //initialiseren
 
     case 1: //autonome mode
-      digitalWrite(motorrechtsdirection, HIGH);
-      digitalWrite(motorlinksdirection, HIGH);
-      for (int x = 0; x < 200; x++) {
-        digitalWrite(motorrechtsstep, HIGH);
-        digitalWrite( motorlinksstep, HIGH);
-        delayMicroseconds(1000);
-        digitalWrite(motorrechtsstep, LOW);
-        digitalWrite(motorlinksstep, LOW);
-        delayMicroseconds(1000);
-      }
 
-      digitalWrite(motorrechtsdirection, HIGH);
-      digitalWrite(motorlinksdirection, LOW);
-      for (int x = 0; x < 98; x++) {
-        digitalWrite(motorrechtsstep, HIGH);
-        digitalWrite( motorlinksstep, HIGH);
-        delayMicroseconds(1000);
-        digitalWrite(motorrechtsstep, LOW);
-        digitalWrite(motorlinksstep, LOW);
-        delayMicroseconds(1000);
-      }
     case 2: //volgmode
 
     case 3: //sturen
@@ -122,17 +110,205 @@ void loop() {
   }
 
 }
-
+trigPinleft 42
+#define echoPinleft 44
+#define trigPinfront 50
+#define echoPinfront 52
+#define trigPinright 38
+#define echoPinright 40
+#define trigPinback 46
+#define echoPinback 48
 void reset() {
 
 }
 
-void noodstopmode() {
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  while (1) {
+void bomen() {
 
+}
+void volgen() {
+
+}
+
+long durationleft;
+long durationfront;
+long durationright;
+long durationback;
+unsigned long timing;
+unsigned long current;
+int distanceleft;
+int distancefront;
+int distanceright;
+int distanceback;
+
+void ultrasoonpersoon() {
+
+  digitalWrite(echoPinleft, LOW);
+  digitalWrite(trigPinleft, HIGH);
+  //tijd
+  digitalWrite(trigPin
+  duratonleft=pulseIn(echoPinleft,HIGH);
+  digitalWrite(trigPinfront);
+  digitalWrite(echoPinfront);
+
+  digitalWrite(trigPinright);
+  digitalWrite(echoPinright);
+
+  digitalWrite(trigPinback);
+  digitalWrite(echoPinback);
+
+  digitalWrite();
+  digitalWrite();
+}
+
+
+void rechtdoor(int richting) {
+  int i = 0;
+  int a = 0;
+  int b = 0;
+  int current;
+  if (richting == 1) {
+    digitalWrite(motorrechtsdirection, HIGH);
+    digitalWrite(motorlinksdirection, LOW);
+  }
+  else {
+    digitalWrite(motorrechtsdirection, LOW);
+    digitalWrite(motorlinksdirection, HIGH);
+  }
+  timing = micros();
+  current = timing;
+  while (i != 1) {
+    timing = micros();
+    // Serial.println("fase 1");
+    // Serial.println(timing);
+    // Serial.println(i);
+    digitalWrite(motorrechtsstep, HIGH);
+    digitalWrite(motorlinksstep, HIGH);
+    if (timing - current >= 3000 and a == 0) {
+      //Serial.println("fase 2");
+      //Serial.println(timing);
+      b = 1;
+      a = 1;
+      digitalWrite(motorrechtsstep, LOW);
+      digitalWrite(motorlinksstep, LOW);
+    }
+    timing = micros();
+    if (timing - current >= 6000 and b == 1) {
+      i++;
+      b = 0;
+      a = 0;
+      //Serial.println("fase 3");
+      // Serial.println(timing);
+      current = timing;
+    }
+  }
+}
+
+
+
+void sturen(int kant) {
+  int i = 0;
+  int a = 0;
+  int b = 0;
+  int current;
+  timing = micros();
+  current = timing;
+  // Serial.println("fase 1");
+  // Serial.println(timing);
+  // Serial.println(i);
+  if (kant == 1) {
+    digitalWrite(motorrechtsdirection, HIGH);
+    digitalWrite(motorlinksdirection, HIGH);
+  }
+  else {
+    digitalWrite(motorrechtsdirection, LOW);
+    digitalWrite(motorlinksdirection, LOW);
+  }
+  digitalWrite(motorrechtsstep, HIGH);
+  digitalWrite(motorlinksstep, HIGH);
+  while (i <= bocht) {
+    if (timing - current >= 3000 and a == 0) {
+      //Serial.println("fase 2");
+      //Serial.println(timing);
+      b = 1;
+      a = 1;
+      digitalWrite(motorrechtsstep, LOW);
+      digitalWrite(motorlinksstep, LOW);
+    }
+    timing = micros();
+    if (timing - current >= 6000 and b == 1) {
+      i++;
+      b = 0;
+      a = 0;
+      //Serial.println("fase 3");
+      // Serial.println(timing);
+      current = timing;
+    }
+    // if (i == 105) {
+    //   a = 3;
+    //  b = 3;
+
+  }
+}
+
+
+
+
+
+
+void iets() {
+  digitalWrite(motorrechtsdirection, HIGH);
+  digitalWrite(motorlinksdirection, HIGH);
+  for (int x = 0; x < 200; x++) {
+    digitalWrite(motorrechtsstep, HIGH);
+    digitalWrite( motorlinksstep, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(motorrechtsstep, LOW);
+    digitalWrite(motorlinksstep, LOW);
+    delayMicroseconds(2000);
+  }
+  /*
+        digitalWrite(motorrechtsdirection, HIGH);
+        digitalWrite(motorlinksdirection, LOW);
+        for (int x = 0; x < 98; x++) {
+          digitalWrite(motorrechtsstep, HIGH);
+          digitalWrite( motorlinksstep, HIGH);
+          delayMicroseconds(1000);
+          digitalWrite(motorrechtsstep, LOW);
+          digitalWrite(motorlinksstep, LOW);
+          delayMicroseconds(1000);
+        }
+  */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void noodstopmode() {
+  int tel = 0;
+  digitalWrite(motorrechtsstep, LOW);
+  digitalWrite(motorlinksstep, LOW);
+  digitalWrite(motorrechtsdirection, LOW);
+  digitalWrite(motorlinksdirection, LOW);
+  while (digitalRead(noodstop) == LOW) {
+    tel++;
+    if (tel == 500) {
+      digitalWrite(buzzer, HIGH);
+    }
+    if (tel == 1000) {
+      tel = 0;
+      digitalWrite(buzzer, LOW);
+    }
   }
 }
--- Stepper_motor_test/Stepper_motor_test.ino
+++ Stepper_motor_test/Stepper_motor_test.ino
@@ -6,23 +6,30 @@
 // defines pins numbers
 const int stepPin = 3;
 const int dirPin = 4;
+const int stepPin1 = 5;
+const int dirPin1 = 6;
 
 void setup() {
   // Sets the two pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
 }
 void loop() {
   digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+  digitalWrite(dirPin1, HIGH); 
   // Makes 200 pulses for making one full cycle rotation
-  for (int x = 0; x < 2000000; x++) {
+  while(1) {
     digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin1, HIGH);
     delayMicroseconds(1000);
     digitalWrite(stepPin, LOW);
+    digitalWrite(stepPin1, LOW);
     delayMicroseconds(1000);
   }
   
-  delay(1000); // One second delay
+  /*delay(1000); // One second delay
 
   digitalWrite(dirPin, LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
@@ -32,5 +39,5 @@ void loop() {
     digitalWrite(stepPin, LOW);
     delayMicroseconds(1000);
   }
-  delay(1000);
+  delay(1000);*/
 }
